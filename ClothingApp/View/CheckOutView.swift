d80e97cf9546bd2d79afcfd664df2a8dcde36354
shadhani <shadhanir@gmail.com>
2024-03-19 10:46:33 +0530

    wwwqeqweq

---
+++ ClothingApp/View/CheckOutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckOutView.swift
+//  ClothingApp
+//
+//  Created by NIBMPC04PC06 on 2024-03-19.
+//
+
+import SwiftUI
+
+struct CheckOutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckOutView()
+}
