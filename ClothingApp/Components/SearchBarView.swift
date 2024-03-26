9cf9124205ede8fd8f56c4e1e1ced670f1baab7d
tamashathiruni <thirunitamasha@gmail.com>
2024-03-26 12:20:10 +0530

    new

---
+++ ClothingApp/Components/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  ClothingApp
+//
+//  Created by NIBM-LAB04-PC04 on 2024-03-26.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
